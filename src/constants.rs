979b38e78687adabc794baa8017042715f932ef4
cnerylozada <cnerylozada@gmail.com>
2025-10-19 08:23:52 -0500

    feat: create constants file

---
+++ src/constants.rs
@@ -0,0 +1,3 @@
+pub const GLOBAL_PREXIF: &str = "/api/v1";
+
+pub const SUPABASE_SESSION_POOLER: &str = "SUPABASE_SESSION_POOLER";
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use axum::Router;
-use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+use serde::Serialize;
+use sqlx::{FromRow, Pool, Postgres, postgres::PgPoolOptions};
 use std::env;
 
+mod constants;
 mod controllers;
 mod routes;
+use constants::{GLOBAL_PREXIF, SUPABASE_SESSION_POOLER};
 use routes::main_router;
 
-const GLOBAL_PREXIF: &str = "/api/v1";
-
 async fn database_connection() -> Result<Pool<Postgres>, sqlx::Error> {
-    let database_url =
-        env::var("SUPABASE_SESSION_POOLER").expect("SUPABASE_SESSION_POOLER in not found");
+    let database_url = env::var(SUPABASE_SESSION_POOLER)
+        .expect(format!("{} env is not found", SUPABASE_SESSION_POOLER).as_str());
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -19,15 +20,26 @@ async fn database_connection() -> Result<Pool<Postgres>, sqlx::Error> {
     Ok(pool)
 }
 
+#[derive(Debug, Serialize, FromRow)]
+struct Userx {
+    name: String,
+    age: i64,
+}
+
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
     let pool = database_connection().await.unwrap();
 
-    let app = Router::new().nest(GLOBAL_PREXIF, main_router());
+    let users = sqlx::query_as::<_, Userx>(r#"SELECT name, age FROM "USERS""#)
+        .fetch_all(&pool)
+        .await
+        .unwrap();
 
-    // run our app with hyper, listening globally on port 3000
+    println!("Users from database: {:?}", users);
+
+    let app = Router::new().nest(GLOBAL_PREXIF, main_router());
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
